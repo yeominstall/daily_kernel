f7092bb99e55a3b519873d763077c34fb39807d3
yminn13 <y.minn13@gmail.com>
2016-08-11 22:04:49 +0900

    20160811 kernel/sched/sched.h/sturct rq - (lost comment)

---
+++ sched_s_k.h
@@ -0,0 +1,120 @@
+/* kernel/sched/sched.h */
+/* linux kernel 3.10 */
+/* struct rq (run queue) */
+
+/* 
+ * This is the main, per-CPU runqueue data structure.
+ * ,,,
+ */
+
+ struct rq {
+ 	raw_spinlock_t lock;
+
+	unsigned int nr_running;
+	#define CPU_LOAD_IDX_MAX 5
+	unsigned long cpu_load[CPU_LOAD_IDX_MAX];
+	unsigned long last_load_update_tick;
+#ifdef CONFIG_NO_HZ_COMMON
+	u64 nohz_stamp;
+	unsigned long nohz_flags;
+#endif
+#ifdef CONFIG_NO_HZ_FULL
+	unsigned long last_sched_tick;
+#endif
+	int skip_clock_update;
+
+	struct load_weight load;
+	unsigned long nr_load_updates;
+	u64 nr_switches;
+
+	struct cfs_rq cfs;
+	struct rt_rq rt;
+
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	struct list_head leaf_cfs_rq_list;
+#ifdef CONFIG_SMP
+	unsigned long h_load_throttle;
+#endif
+#endif
+
+#ifdef CONFIG_RT_GROUP_SCHED
+	struct list_head leaf_rt_rq_list;
+#endif
+
+	unsigned long nr_uninterruptible;
+	
+	struct task_struct *curr, *idle, *stop;
+	unsigned long next_balance;
+	struct mm_struct *prev_mm;
+
+	u64 clock;
+	u64 clock_task;
+
+	atomic_t nr_iowait;
+
+#ifdef CONFIG_SMP
+	struct root_domain *rd;
+	struct sched_domain *sd;
+
+	unsigned long cpu_power;
+
+	unsigned char idle_balance;
+
+	int post_schedule;
+	int active_balance;
+	int push_cpu;
+	struct cpu_stop_work active_balance_work;
+
+	int cpu;
+	int online;
+
+	struct list_head cfs_tasks;
+
+	u64 rt_avg;
+	u64 age_stamp;
+	u64 idle_stamp;
+	u64 avg_idle;
+#endif
+
+#ifdef CONFIG_IRQ_TIME_ACCOUNTING
+	u64 prev_irq_time;
+#endif
+#ifdef CONFIG_PARAVIRT
+	u64 prev_steal_time;
+#endif
+#ifdef CONFIG_PARAVIRT_TIME_ACCOUNTING
+	u64 prev_steal_time_rq;
+#endif
+
+	unsigned long calc_load_update;
+	long calc_load_active;
+
+#ifdef CONFIG_SCHED_HRTICK
+#ifdef CONFIG_SMP
+	int hrtick_csd_pending;
+	struct call_single_data hrtick_csd;
+#endif
+	struct hrtimer hrtick_timer;
+#endif
+
+#ifdef CONFIG_SCHEDSTATS
+	struct sched_info rq_sched_info;
+	unsigned long long rq_cpu_time;
+
+	unsigned int yld_count;
+
+	unsigned int sched_count;
+	unsigned int sched_goidle;
+
+	unsigned int ttwu_count;
+	unsigned int ttwu_local;
+#endif
+
+#ifdef CONFIG_SMP
+	struct llist_head wake_list;
+#endif
+
+	struct sched_avg avg;
+};
+
+ }
